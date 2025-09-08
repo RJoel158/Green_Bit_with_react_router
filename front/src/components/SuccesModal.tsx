67d1450053b69d5b6dbd3549c320188ee11adca2
smm222-cyber <smm0034570@est.univalle.edu>
2025-09-07 20:55:54 -0400

    Diseño modal registro

---
+++ front/src/components/SuccesModal.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./SuccessModal.css";
+import emailLogo from "../assets/icons/email-logo.svg";
+
+interface SuccessModalProps {
+  title: string;
+  message: string;
+  redirectUrl: string;
+}
+
+const SuccessModal: React.FC<SuccessModalProps> = ({ title, message, redirectUrl }) => {
+  return (
+    <div className="modal-overlay d-flex justify-content-center align-items-center">
+      <div className="modal-box p-4 text-center">
+        <img src={emailLogo} alt="Email" className="modal-icon mb-3" />
+        <h2 className="mb-2">{title}</h2>
+        <h3 className="mb-3">{message}</h3>
+        <div className="d-flex justify-content-end">
+          <button
+            className="btn modal-button"
+            onClick={() => (window.location.href = redirectUrl)}
+          >
+            Aceptar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessModal;
